2093cbf552a631df61e51c777e2e528697981022
Soham_Kadam <95161403+ksoham2003@users.noreply.github.com>
2023-08-30 12:48:27 +0530

    Add files via upload

---
+++ Metacharacters.py
@@ -0,0 +1,18 @@
+import re
+
+print("\"Hello\"")
+
+txt = "This is python programming" 
+print(re.findall("[p-t]", txt))
+
+print(bool(re.findall("^ python",txt)))
+
+print(bool(re.findall("programming$",txt)))
+
+print(re.findall("pyt..n",txt))
+
+print(re.findall("pro*gramming",txt))
+
+print(re.findall("prog+ramming",txt))
+
+print(re.findall("program?ing",txt))
