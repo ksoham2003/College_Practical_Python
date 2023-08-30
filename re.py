8cf5e38c994cd45cbe131319ec82a3973e6a0581
Soham_Kadam <95161403+ksoham2003@users.noreply.github.com>
2023-08-30 12:30:24 +0530

    Update re.py

--- re.py
+++ re.py
@@ -5,3 +5,9 @@ match = re.search('program',s)
 print('Start index', match.start())
 print('End Index ',match.end())
 print(match)
+
+str = "Hello my no. is 1234567890 & another no. is 9876543210"
+
+regex = '\d+'
+match = re.findall(regex,str)
+print(match)
